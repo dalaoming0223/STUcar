140246c04161175eec12531c4796b6ca05e47d9d
dalaoming <779507434@qq.com>
2019-12-15 11:29:20 +0800

    日常更新
    如题12.14

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -1,18 +1,18 @@
 //index.js
 const app = getApp()
-
+const db = wx.cloud.database();
+const _ = db.command;
 Page({
   data: {
     modalShow: false,
     avatarUrl: './user-unlogin.png',
-    userInfo: {},
     logged: false,
     takeSession: false,
     requestResult: '',
     StatusBar: app.globalData.StatusBar,
     CustomBar: app.globalData.CustomBar,
     Custom: app.globalData.Custom,
-    cardCur: 0,
+    cardCur: null,
     swiperList: [{
       id: 0,
       type: 'image',
@@ -48,7 +48,8 @@ Page({
     statusBarHeight: 0,//状态栏高度
     titleBarHeight: 0,//标题栏高度
     navBarHeight: 0,//导航栏高度
-    navData: ['人找车', '车找人'],
+
+    navData: ['人找车', '拼车车'],
     currentNavTab: 0,//当前状态
 
     dnName:'CarOwnerRecord',//查询集合列表，默认人找车
@@ -60,6 +61,7 @@ Page({
     hotList: [],
     hotCurrent:0,
 
+    location: ['stu', '高铁站', '机场']
   },
 
   onPublishPingCar() {
@@ -100,20 +102,13 @@ Page({
     })
   },
   onLoad: function() {
+    let _this = this
     if (!wx.cloud) {
       wx.redirectTo({
         url: '../chooseLib/chooseLib',
       })
       return
     }
-    
-    //是否授权登录
-    // if(!app.globalData.userInfo){
-    //   wx.redirectTo({
-    //     url: '',
-    //   })
-    // }
-
     // 获取用户信息
     wx.getSetting({
       success: res => {
@@ -132,9 +127,61 @@ Page({
         }
       }
     })
-    
+    _this.onGetSystemInfo();
+    _this.onGetOpenid();
+    wx.showLoading({
+      title: '加载中...',
+    });
+    _this.onGetHotNews(10);//获取热点新闻
+    _this.addData(1, _this.data.currentNavTab);//第一个参数页数，第二个参数分类
   },
 
+  /**
+   * 获取设备信息
+   */
+  onGetSystemInfo:function(){
+    let _this = this
+    // 因为很多地方都需要用到，所有保存到全局对象中
+    if (app.globalData.statusBarHeight && app.globalData.titleBarHeight)    {
+      _this.setData({
+        statusBarHeight: app.globalData.statusBarHeight,
+        titleBarHeight: app.globalData.titleBarHeight,
+        navBarHeight: app.globalData.navBarHeight,
+        windowHeight: app.globalData.windowHeight,
+        windowWidth: app.globalData.windowWidth
+      });
+    } else {
+      console.log(11111);
+      wx.getSystemInfo({
+        success: function (res) {
+          console.log(res);
+          if (!app.globalData) {
+            app.globalData = {}
+          }
+          //这里默认iOS安卓导航栏都是44;
+          app.globalData.titleBarHeight = 44;
+          app.globalData.statusBarHeight = res.statusBarHeight;
+          app.globalData.windowHeight = res.windowHeight;
+          app.globalData.windowWidth = res.windowWidth;
+          app.globalData.navBarHeight = res.statusBarHeight+44;
+          _this.setData({
+            statusBarHeight: app.globalData.statusBarHeight,
+            titleBarHeight: app.globalData.titleBarHeight,
+            navBarHeight: app.globalData.navBarHeight,
+            windowHeight: app.globalData.windowHeight,
+            windowWidth: app.globalData.windowWidth
+          });
+        },
+        failure() {
+          that.setData({
+            statusBarHeight: 0,
+            titleBarHeight: 0
+          });
+        }
+      })
+    }
+  },
+  
   onGetUserInfo: function(e) {
     if (!this.data.logged && e.detail.userInfo) {
       this.setData({
@@ -144,7 +191,9 @@ Page({
       })
     }
   },
-
+  /**
+   * 获取_openid
+   */
   onGetOpenid: function () {
     // 调用云函数
     wx.cloud.callFunction({
@@ -156,61 +205,193 @@ Page({
       },
       fail: err => {
         console.error('[云函数] [login] 调用失败', err)
-        wx.navigateTo({
-          url: '../deployFunctions/deployFunctions',
-        })
       }
     })
   },
 
-  // 上传图片
-  doUpload: function () {
-    // 选择图片
-    wx.chooseImage({
-      count: 1,
-      sizeType: ['compressed'],
-      sourceType: ['album', 'camera'],
-      success: function (res) {
-
-        wx.showLoading({
-          title: '上传中',
-        })
-
-        const filePath = res.tempFilePaths[0]
-        
-        // 上传图片
-        const cloudPath = 'my-image' + filePath.match(/\.[^.]+?$/)[0]
-        wx.cloud.uploadFile({
-          cloudPath,
-          filePath,
-          success: res => {
-            console.log('[上传文件] 成功：', res)
-
-            app.globalData.fileID = res.fileID
-            app.globalData.cloudPath = cloudPath
-            app.globalData.imagePath = filePath
-            
-            wx.navigateTo({
-              url: '../storageConsole/storageConsole'
-            })
-          },
-          fail: e => {
-            console.error('[上传文件] 失败：', e)
-            wx.showToast({
-              icon: 'none',
-              title: '上传失败',
-            })
-          },
-          complete: () => {
-            wx.hideLoading()
-          }
-        })
-
-      },
-      fail: e => {
-        console.error(e)
-      }
+  /**
+   * 获取热点快报
+   */
+  onGetHotNews:function(n){
+    let _this = this;
+    db.collection('RoadInfo').where({
+      status: _.neq(0)
     })
+    .orderBy('sendTime','desc')
+    .limit(n)
+    .get({
+      success: function (res) {
+        console.log(res.data);
+        let list = res.data
+        _this.setData({
+          hotList: list
+        });
+      },
+      fail: console.error
+    })
+  },
+  /**
+   * 导航列表点击
+   */
+  switchNav(event) {
+    console.log('上方分类导航返回值')
+    console.log(event) 
+    console.log('上方分类导航返回值')
+    let _this = this;
+
+    console.log(_this.data.currentNavTab)
+    let cur = event.currentTarget.dataset.current;
+    //每个tab选项宽度占1/5
+    // let singleNavWidth = this.data.windowWidth / 5;
+    //tab选项居中                            
+    // _this.setData({
+    //   navScrollLeft: (cur - 2) * singleNavWidth
+    // })
+    if (_this.data.currentNavTab == cur) {
+      return false;
+    } else {
+      this.setData({
+        currentNavTab: cur,
+        list:[]
+      });
+    //加载数据
+      _this.addData(1, _this.data.currentNavTab);
+    }
+    // if (){
+
+    // }
+    // _this.addData(1, cur);
+    // console.log('此时cur')
+    // console.log(cur)
+    // console.log('此时cur')
+    // _this.addData(1, cur);
+  },
+  
+  /**
+   * 获取有效的拼车信息
+   */
+  addData: function (n,s) {//第一个参数页数，第二个参数分类
+    let _this = this;
+    let startTime = _this.startTime();
+    let endTime = _this.endTime();
+    switch (s){
+      case 0:
+        _this.setData({
+          dnName : "CarOwnerRecord"
+        });
+        break;
+      case 1:
+        _this.setData({
+          dnName : "PassengersRecord"
+        })
+        break;
+      case 4:
+        // _this.setData({
+        //   dnName : ""
+        // })
+        break;
+    }
+    wx.showLoading({
+      title: '加载中...',
+    });
+    //按照时间查询，规则开始当前时间60分钟前 到明天24：00；
+    wx.cloud.callFunction({
+      name: 'queryInfo',
+      data:{
+        dbName: _this.data.dnName,
+        pageIndex:n,
+        pageSize:15,
+        filter:{},
+        startTime: _this.startTime(),
+        endTime: _this.endTime()
+      }
+    }).then(res => {
+      console.log(res);
+      _this.setData({
+        isLodding: false,
+        list: res.result.data,
+        pageIndex: _this.data.pageIndex+1,
+        hasMore: res.result.hasMore
+      });
+      wx.hideLoading();
+    });
+  },
+  /**
+   * 下拉刷新
+   */
+  onPullDownRefresh:function(){
+    let _this = this;
+    
+    //显示刷新图标
+    wx.showLoading({
+      title: '加载中...',
+    });
+    _this.setData({
+      pageIndex:1
+    });
+    _this.onGetHotNews(10);//获取热点新闻
+    _this.addData(1,_this.data.currentNavTab);
+    //停止刷新，页面回单
+    wx.stopPullDownRefresh();
+  },
+  
+  /**
+   * 上拉加载更多
+   */
+  onReachBottom:function(){
+    let _this = this;
+    if(!_this.data.hasMore) return;//没有下一页了
+
+    let startTime = _this.startTime();
+    let endTime = _this.endTime();
+    //按照时间查询，规则开始当前时间60分钟前 到明天24：00；
+    wx.cloud.callFunction({
+      name: 'queryInfo',
+      data: {
+        dbName: _this.data.dnName,
+        pageIndex: _this.data.pageIndex,
+        pageSize: 15,
+        filter:{},
+        startTime: _this.startTime(),
+        endTime: _this.endTime()
+      }
+    }).then(res => {
+      console.log('有效评车信息')
+      console.log(res);
+      _this.setData({
+        list: _this.data.list.concat(res.result.data),
+        pageIndex: _this.data.pageIndex+1,
+        hasMore: res.result.hasMore
+      });
+    });
+  },
+  /**
+   * 热点新闻切换
+   */
+  hotSwiperChange:function(e){
+    this.setData({
+      hotCurrent: e.detail.current
+    });
+  },
+  /**
+   * 获取60分钟前时间戳
+   */
+  startTime:function(){
+    let day = new Date()
+    let strTime = day.getTime() - 1 * 60 * 60 * 1000;
+    console.log(strTime)
+    return strTime;
+  },
+  /**
+   * 获取次日凌晨时间戳
+   */
+  endTime:function(){
+    // 获取当天 0 点的时间戳
+    let oneTime = new Date(new Date().setHours(0, 0, 0, 0)) / 1000;
+    //次日凌晨时间戳
+    let threeTime = oneTime + 86400 *2;
+    console.log(threeTime*1000)
+    return threeTime*1000;
   },
 
 })
--- miniprogram/pages/profile/profile.js
+++ miniprogram/pages/profile/profile.js
@@ -1,10 +1,12 @@
 // miniprogram/pages/profile/profile.js
+const app = getApp()
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+    userInfo: null,
     visitTotal: 20,
     starCount: 20,
     forksCount: 20
@@ -14,21 +16,33 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    let that = this
+    // 加载用户信息
+    wx.getUserInfo({
+      withCredentials: false,
+      lang: 'zh_CN',
+      success: function (resp) {
+        that.setData({
+          userInfo: resp.userInfo
+        });
+      }
+    })
+    console.log('====')
+    console.log(app.globalData)
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+    console.log(app.globalData)
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    // console.log(app.globalData)
   },
 
   /**
